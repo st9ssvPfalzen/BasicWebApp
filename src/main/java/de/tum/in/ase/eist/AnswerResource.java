5cd55819f3260c596cf3605b219e2cb8757bde6a
SusanneTaferner <susianne.taferner@tum.de>
2022-07-14 14:53:19 +0200

    irrelevant

--- src/main/java/de/tum/in/ase/eist/AnswerResource.java
+++ src/main/java/de/tum/in/ase/eist/AnswerResource.java
@@ -21,8 +21,6 @@ public class AnswerResource {
 		this.queryProcessor = queryProcessor;
 	}
 
-
-
 	@GetMapping("*")
 	public ResponseEntity<String> answer(@RequestParam("q") String query) {
 		logger.info("Received query \"{}\"", query);
